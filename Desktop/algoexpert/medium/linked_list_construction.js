cd48dc0f68e2e7710f095bf1d9f054b9a2fe7207
Kathiravan K <kathirr007@users.noreply.github.com>
2021-08-01 23:00:31 +0530

    linked list construction

---
+++ Desktop/algoexpert/medium/linked_list_construction.js
@@ -0,0 +1,116 @@
+// 6/29/21
+// looked at solution
+
+// Feel free to add new properties and methods to the class.
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // Time: O(1) | Space: O(1)
+  setHead(node) {
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
+    this.insertBefore(this.head, node);
+  }
+
+  // Time: O(1) | Space: O(1)
+  setTail(node) {
+    if (this.tail === null) {
+      this.setHead(node);
+      return;
+    }
+    this.insertAfter(this.tail, node);
+  }
+
+  // Time: O(1) | Space: O(1)
+  insertBefore(node, nodeToInsert) {
+    if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node.prev;
+    nodeToInsert.next = node;
+    if (node.prev === null) {
+      this.head = nodeToInsert;
+    } else {
+      node.prev.next = nodeToInsert;
+    }
+    node.prev = nodeToInsert;
+  }
+
+  // Time: O(1) | Space: O(1)
+  insertAfter(node, nodeToInsert) {
+    if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+    this.remove(nodeToInsert);
+    nodeToInsert.prev = node;
+    nodeToInsert.next = node.next;
+    if (node.next === null) {
+      this.tail = nodeToInsert;
+    } else {
+      node.next.prev = nodeToInsert;
+    }
+    node.next = nodeToInsert;
+  }
+
+  // Time: O(p) | Space: O(1)
+  insertAtPosition(position, nodeToInsert) {
+    if (position === 1) {
+      this.setHead(nodeToInsert);
+      return;
+    }
+    let node = this.head;
+    let currentPosition = 1;
+    while (node !== null && currentPosition++ !== position) node = node.next;
+    if (node !== null) {
+      this.insertBefore(node, nodeToInsert);
+    } else {
+      this.setTail(nodeToInsert);
+    }
+  }
+
+  // Time: O(n) | Space: O(1)
+  removeNodesWithValue(value) {
+    let node = this.head;
+    while (node !== null) {
+      const nodeToRemove = node;
+      node = node.next;
+      if (nodeToRemove.value === value) this.remove(nodeToRemove);
+    }
+  }
+
+  // Time: O(1) | Space: O(1)
+  remove(node) {
+    if (node === this.head) this.head = this.head.next;
+    if (node === this.tail) this.tail = this.tail.prev;
+    this.removeNodeBindings(node);
+  }
+
+  // Time: O(n) | Space: O(1)
+  containsNodeWithValue(value) {
+    let node = this.head;
+    while (node !== null && node.value !== value) node = node.next;
+    return node !== null;
+  }
+
+  removeNodeBindings(node) {
+    if (node.prev !== null) node.prev.next = node.next;
+    if (node.next !== null) node.next.prev = node.prev;
+    node.prev = null;
+    node.next = null;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
+exports.DoublyLinkedList = DoublyLinkedList;
