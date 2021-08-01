ab5cf72eed5d86f38ac5cb22a160014c8c8fc4fd
Kathiravan K <kathirr007@users.noreply.github.com>
2021-08-02 00:24:28 +0530

    apartment hunting

---
+++ Desktop/algoexpert/very-hard/apartment_hunting.js
@@ -0,0 +1,38 @@
+// O(b^2*r) time | O(b) space - where b is the number of blocks and r is the number of requirements
+function apartmentHunting(blocks, reqs) {
+  const maxDistancesAtBlocks = new Array(blocks.length).fill(-Infinity);
+  for (let i = 0; i < blocks.length; i++) {
+    for (const req of reqs) {
+      let closestReqDistance = Infinity;
+      for (let j = 0; j < blocks.length; j++) {
+        if (blocks[j][req]) {
+          closestReqDistance = Math.min(
+            closestReqDistance,
+            distanceBetween(i, j)
+          );
+        }
+      }
+      maxDistancesAtBlocks[i] = Math.max(
+        maxDistancesAtBlocks[i],
+        closestReqDistance
+      );
+    }
+  }
+  return getIdxAtMinValue(maxDistancesAtBlocks);
+}
+function getIdxAtMinValue(array) {
+  let idxAtMinValue = 0;
+  let minValue = Infinity;
+  for (let i = 0; i < array.length; i++) {
+    const currentValue = array[i];
+    if (currentValue < minValue) {
+      minValue = currentValue;
+      idxAtMinValue = i;
+    }
+  }
+  return idxAtMinValue;
+}
+function distanceBetween(a, b) {
+  return Math.abs(a - b);
+}
+exports.apartmentHunting = apartmentHunting;
